c496c0366dbd0b3ca6da96a6d98106e11f5d2bc8
Mihir Hirave <54632347+mihirhirave@users.noreply.github.com>
2023-06-06 23:26:33 +0530

    Update Py6.py

--- Py6.py
+++ Py6.py
@@ -1,4 +1,24 @@
+'''
+Question-6 [Python]
+Using the data from Question 3, write code to analyze the data and answer the following questions 
 
+Note:-
+1. Draw plots to demonstrate the analysis for the following questions for better visualizations.
+2. Write code comments wherever required for code understanding
+
+Insights to be drawn -
+
+* Get all Pokemons whose spawn rate is less than 5%
+* Get all Pokemons that have less than 4 weaknesses
+* Get all Pokemons that have no multipliers at all
+* Get all Pokemons that do not have more than 2 evolutions
+* Get all Pokemons whose spawn time is less than 300 seconds.
+
+Note - spawn time format is "05:32”, so assume “minute: second” format and perform the analysis.
+
+* Get all Pokemon who have more than two types of capabilities
+
+'''
 
 ##Answer_6
 
