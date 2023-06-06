785d291d888aa2e2469b2677b628d0cf95f5374e
Mihir Hirave <54632347+mihirhirave@users.noreply.github.com>
2023-06-06 23:24:35 +0530

    Update Py2.py

--- Py2.py
+++ Py2.py
@@ -1,4 +1,22 @@
+'''
 
+Question 2: -
+
+Consider a string to be valid if all characters of the string appear the same number of times. It is also valid if
+he can remove just one character at the index in the string, and the remaining characters will occur the same
+number of times. Given a string, determine if it is valid. If so, return YES , otherwise return NO .
+
+Note - You have to write at least 2 additional test cases in which your program will run successfully and provide
+an explanation for the same.
+
+Example input 1 - s = “abc”. This is a valid string because frequencies are { “a”: 1, “b”: 1, “c”: 1 }
+Example output 1- YES
+
+Example input 2 - s “abcc”. This string is not valid as we can remove only 1 occurrence of “c”. That leaves
+character frequencies of { “a”: 1, “b”: 1 , “c”: 2 }
+Example output 2 - NO
+
+'''
 
 ##Answer_2
 
