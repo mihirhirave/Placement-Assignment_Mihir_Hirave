c7db6743164ffc3c3ea0d13eb44e4ae5f610b9db
Mihir Hirave <54632347+mihirhirave@users.noreply.github.com>
2023-06-06 23:25:35 +0530

    Update Py4.py

--- Py4.py
+++ Py4.py
@@ -1,3 +1,15 @@
+'''
+
+Question 4 -
+
+Write a program to download the data from the link given below and then read the data and convert the into
+the proper structure and return it as a CSV file.
+
+Link - https://data.nasa.gov/resource/y77d-th95.json
+
+Note - Write code comments wherever needed for code understanding.
+
+'''
 
 ##Answer_4
 
